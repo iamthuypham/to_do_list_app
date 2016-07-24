21f14261b5fc180226a6db2156c35c9ef7df9bed
Thuy Pham <tpham045@hotmail.com>
2016-07-24 18:32:48 +0000

    Remove unnessary methods in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,7 @@
 class UsersController < ApplicationController
   def new
-     @user = User.new
   end
   def create
-     @user = User.new
-     @user.name = params[:user][:name]
-     @user.email = params[:user][:email]
-     @user.password = params[:user][:password]
-     @user.password_confirmation = params[:user][:password_confirmation]
-     
-     if @user.save
-       flash[:notice] = "Welcome to Blocitoff #{@user.name.capitalize}!"
-       user_session(@user)
-       redirect_to root_path
-     else
-       flash.now[:alert] = "There was an error creating your account. Please try again."
-       render :new
-     end
   end
   def show
     @user = User.find(params[:id])
