779cd03d21123c3d89600334513ca3344985551a
Thuy Pham <tpham045@hotmail.com>
2016-07-26 01:39:47 +0000

    Add SendGrid, Figaro. Able to receive confirmation email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  #Email Confirmation with Devise
+  config.action_mailer.default_url_options = { :host => 'damp-journey-47405.herokuapp.com' }
 end
