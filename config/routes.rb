d8f44fc050e438a2bb95e1bac21ed777cc487541
Thuy Pham <tpham045@hotmail.com>
2016-07-28 02:15:35 +0000

    Create and Edit items, view current user item and all items

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-
   devise_for :users
   resources :users, only: [:show] do
     resources :items, only: [:new, :create, :show, :index, :edit]
   end
-  
+
   get 'welcome/index'
   root 'welcome#index'
 
