58002ae8ea5d00ceb51d913ebb0555f92333f7cc
Thuy Pham <tpham045@hotmail.com>
2016-07-30 01:24:43 +0000

    Ability to mark complete

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
     resources :items
   end
 
+  # authenticated :user do
+  #   root to: 'welcome#index', as: :authenticated_root
+  # end
+  # root to: redirect('/users/sign_in')
   
   # authenticated :user do
   #   root to: 'welcome#index', as: :authenticated_root
